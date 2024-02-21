0c4e4726115c607fa2bb80894daf8342b2a68e5a
Kevin Soffa <84532185+KevinSoffa@users.noreply.github.com>
2024-02-21 19:09:15 -0300

    trazendo tarefas do DB

--- controllers/TaskController.mjs
+++ controllers/TaskController.mjs
@@ -6,7 +6,24 @@ export default class TaskController{
         res.render('tasks/create')
     }
 
-    static showTasks(req, res) {
-        res.render('tasks/all')
+    // Criando tarefa [POST]
+    static async createTaskSave(req, res) {
+        const task = {
+            // pegando dados do HTML
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+        // Salvando tarefa
+        await Task.create(task)
+
+        res.redirect('/tasks')
+    }
+
+    // Resgatando tarefas do DB [GET] 
+    static async showTasks(req, res) {
+        const tasks = await Task.findAll({raw: true})
+
+        res.render('tasks/all', {tasks})
     }
 }
--- routes/tasksRoutes.mjs
+++ routes/tasksRoutes.mjs
@@ -1,11 +1,14 @@
-import express from 'express';
 import TaskController from '../controllers/TaskController.mjs';
+import express from 'express';
 
 
 const router = express.Router()
 
 //Rotas
 router.get('/', TaskController.showTasks)
+
+//Rota add
+router.post('/add', TaskController.createTaskSave)
 router.get('/add', TaskController.createTask)
 
 export default router;
